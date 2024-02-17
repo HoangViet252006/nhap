07e7d42abc5dbe18c64ad32b5c5cda4d4b96609a
VietHQ <viethq@VietHQs-MacBook-Air.local>
2024-02-17 23:21:24 +0700

    upload source code

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ const content = document.getElementById('content');
 const backGround = document.getElementById('bg');
 
 
+const btnPlay = document.getElementById("btnPlay");
+const video = document.getElementById("video");
+
+var flag = false;
+// btnPlay.addEventListener("click", function() {
+//     video.style.display = "block";
+//     btnPlay.style.display = "none";
+// });
+
+btnPlay.addEventListener("click", function() {
+    flag = !flag;
+    if (flag == true) {
+        btnPlay.textContent = "ấn là ẩn video đóoooo";
+        video.style.display = "block";
+    } else {
+        btnPlay.textContent = "ấn vào đây điiiiii";
+        video.style.display = "none";
+    }
+});
+
+
 
 letterImage.addEventListener('click', () => {
     content.style.display = 'block';
